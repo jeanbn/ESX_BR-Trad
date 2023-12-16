c934664eed2ba539cc92179e0c209d45a88fd91e
jeanbn <57046909+jeanbn@users.noreply.github.com>
2023-12-16 20:22:30 -0300

    Add files via upload
    Signed-off-by: jeanbn <57046909+jeanbn@users.noreply.github.com>

---
+++ esx_banking/br.lua
@@ -0,0 +1,16 @@
+Locales['br'] = {
+  ['invalid_amount'] = 'Essa é uma quantia inválida de dinheiro',
+  ['deposit_money']  = 'você depositou $%s',
+  ['withdraw_money'] = 'você retirou $%s',
+  ['pincode_money'] = 'você tem um novo código PIN %s',
+  ['transfer_money'] = 'você conseguiu transferir dinheiro $%s ID: %s',
+  ['receive_transfer'] = 'você recebe dinheiro para transferência $%s ID: %s',
+  ['press_e_banking']    = 'pressione [E] para acessar o banco',
+  ['access_bank']    = 'Acesse o banco',
+  ['banking_blip']       = 'Banco',
+  ['cant_do_it']       = "Não posso fazer isso!",
+  ['not_enough_money'] = "Dinheiro insuficiente! Você precisa de %s dinheiro!",
+  ['pincode_not_found'] = "Código PIN inválido",
+  ['pincode_found'] = "Código PIN válido...",
+  ['bank_name'] = "Banco Fleeca"
+}
